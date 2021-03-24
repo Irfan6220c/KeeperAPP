0ffffb9fec7073fba62575ef52f4a03c9c5a5eac
Irfan6220c <45431479+Irfan6220c@users.noreply.github.com>
2021-03-24 23:57:56 +0100

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,21 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-  var d = new Date();
-  var year = d.getFullYear();
-
   return (
     <div>
-      <div>
-        <header className="header">
-          <h1>Keeper </h1>
-        </header>
-      </div>
-      <div className="note">
-        <h1 className="noteh1"> This is a Note title </h1>
-        <p className="notep"> This is content of the note </p>
-      </div>
-      <footer className="footer">
-        <p>Copyright &copy; {year}</p>
-      </footer>
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
     </div>
   );
 }
